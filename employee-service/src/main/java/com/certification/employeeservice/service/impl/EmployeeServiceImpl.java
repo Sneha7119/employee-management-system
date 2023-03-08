1bd0919bd5f2a41d84615f9034cb387d54c3100a
Sneha7119 <snehadevadas9@gmail.com>
2023-03-08 15:03:06 +0530

    Circuit breaker

--- employee-service/src/main/java/com/certification/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/certification/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -6,8 +6,12 @@ import com.certification.employeeservice.dto.EmployeeDto;
 import com.certification.employeeservice.entity.Employee;
 import com.certification.employeeservice.repository.EmployeeRepository;
 import com.certification.employeeservice.service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -23,6 +27,9 @@ public class EmployeeServiceImpl implements EmployeeService {
     private APIClient apiClient;
 
     private ModelMapper modelMapper;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
         Employee savedEmployee = employeeRepository.save(modelMapper.map(employeeDto, Employee.class));
@@ -30,7 +37,10 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
+    //@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     public ApiResponseDto getEmployeeById(Long id) {
+        LOGGER.info("[EmployeeServiceImpl] -> Starting getEmployeeById()");
         Employee fetchedEmployee = employeeRepository.findById(id).get();
 
         /*
@@ -50,6 +60,19 @@ public class EmployeeServiceImpl implements EmployeeService {
                 .block();         // block()  -> specifies synchronous call*/
 
         DepartmentDto departmentDto = apiClient.getDepartmentByCode(fetchedEmployee.getDepartmentCode());
+        return new ApiResponseDto(
+                modelMapper.map(fetchedEmployee, EmployeeDto.class),
+                departmentDto);
+    }
+
+    public ApiResponseDto getDefaultDepartment(Long id, Exception exception) {
+        LOGGER.info("[EmployeeServiceImpl] -> Starting getDefaultDepartment()");
+        Employee fetchedEmployee = employeeRepository.findById(id).get();
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setDepartmentName("Default Dep");
+        departmentDto.setDepartmentCode("DD");
+        departmentDto.setDepartmentDescription("Default Department since Deaprtment service is down");
+
         return new ApiResponseDto(
                 modelMapper.map(fetchedEmployee, EmployeeDto.class),
                 departmentDto);
